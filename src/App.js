d8cec55cf1f69614d78b0859d08c7a44b1841f39
Paulo Riccardo <pauloriccardo@hotmail.com>
2020-08-18 17:29:02 -0300

    Implementação de melhorias no código

--- src/App.js
+++ src/App.js
@@ -1,8 +1,86 @@
 import React, { Component } from 'react';
-import CalculaSalario from './components/CalculaSalario';
+import InputFullSalary from './components/InputFullSalary';
+import { calculateSalaryFrom } from './helper/salary';
+import InputReadOnly from './components/ImputReadOnly';
+import ProportionBar from './components/ProportionBar';
+
+const COLOR_INSS = '#e67e22';
+const COLOR_IRPF = '#C0392B';
+const COLOR_NET_SALARY = '#16A085';
 
 export default class App extends Component {
+    constructor() {
+        super();
+
+        this.state = { fullSalary: 1000 };
+    }
+
+    handleFullSalaryChange = (newValue) => {
+        this.setState({
+            fullSalary: newValue,
+        });
+    };
+
     render() {
-        return <CalculaSalario />;
+        const { fullSalary } = this.state;
+        const salaryObject = calculateSalaryFrom(fullSalary);
+        const {
+            baseINSS,
+            discountINSS,
+            baseIRPF,
+            discountIRPF,
+            netSalary,
+            percentageINSS,
+            percentageIRPF,
+            percentageNetSalary,
+        } = salaryObject;
+
+        return (
+            <div className="container">
+                <h3>Calcule seu salário</h3>
+                <div className="row">
+                    <InputFullSalary
+                        currentValue={fullSalary}
+                        onSalaryChange={this.handleFullSalaryChange}
+                    />
+                </div>
+                <div className="row">
+                    <InputReadOnly label="Base Inss:" value={baseINSS} />
+
+                    <InputReadOnly
+                        label="Desconto Inss:"
+                        value={discountINSS}
+                        percentage={percentageINSS}
+                        color={COLOR_INSS}
+                    />
+
+                    <InputReadOnly label="Base IRPF:" value={baseIRPF} />
+
+                    <InputReadOnly
+                        label="Desconto IRPF:"
+                        value={discountIRPF}
+                        percentage={percentageIRPF}
+                        color={COLOR_IRPF}
+                    />
+
+                    <InputReadOnly
+                        label="Salário Liquido:"
+                        value={netSalary}
+                        percentage={percentageNetSalary}
+                        color={COLOR_NET_SALARY}
+                    />
+                </div>
+                <div>
+                    <ProportionBar
+                        inss={percentageINSS}
+                        irpf={percentageIRPF}
+                        netSalary={percentageNetSalary}
+                        colorINSS={COLOR_INSS}
+                        colorIRPF={COLOR_IRPF}
+                        colorNetSalary={COLOR_NET_SALARY}
+                    />
+                </div>
+            </div>
+        );
     }
 }
--- src/components/Salario.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Salario extends Component {
-    render() {
-        return <input type="number" />;
-    }
-}
